aca2e2992c82c0f9deda28d1c977a2457e8acd5f
Irish <2015081452@ust-ics.mygbiz.com>
2018-12-06 14:50:56 +0800

    Initial commit

---
+++ app/src/main/java/com/irish/salvador/salvadoririshpe2/MainActivity.java
@@ -0,0 +1,88 @@
+package com.irish.salvador.salvadoririshpe2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.widget.TextView;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String FILE_NAME = "pract2.txt";
+    EditText efull;
+    EditText eage;
+    EditText egend;
+    TextView eresu;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        efull = findViewById(R.id.full);
+        eage = findViewById(R.id.age);
+        egend = findViewById(R.id.gend);
+        eresu = findViewById(R.id.resu);
+    }
+    public void iSearch(View v){
+        FileInputStream fis = null;
+
+        try {
+            fis = openFileInput(FILE_NAME);
+            InputStreamReader isr = new InputStreamReader(fis);
+            BufferedReader br = new BufferedReader(isr);
+            StringBuilder sb = new StringBuilder();
+            String text;
+
+            while ((text = br.readLine()) != null) {
+                sb.append(text).append("\n");
+            }
+
+            eresu.setText(sb.toString());
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fis != null) {
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+    public void iSave(View v){
+        String text1 = efull.getText().toString();
+        String text2 = eage.getText().toString();
+        String text3 = egend.getText().toString();
+        String text = (text1 + "\n" + text2 + "\n" + text3);
+        FileOutputStream fos = null;
+
+        try {
+            fos = openFileOutput(FILE_NAME, MODE_PRIVATE);
+            fos.write(text.getBytes());
+            text.getText().clear();
+            Toast.makeText(this, "Saved to " + getFilesDir() + "/" + FILE_NAME,
+                    Toast.LENGTH_LONG).show();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fos != null) {
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
